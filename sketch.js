15d84073e6f4d6860e7f9fb5d369f82fc1c719dc
Arafath76 <ali2frnds@gmail.com>
2020-10-09 20:15:05 +0530

    added

--- sketch.js
+++ sketch.js
@@ -1,9 +1,59 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var ground1,block1,Canvas;
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  engine = Engine.create();
+    world = engine.world;
+  createSprite(400, 290, 50, 50);
+
+  ground1 = new Ground(600,390,1200,20);
+  ground2 = new Ground(300,400,100,20);
+  ground3 = new Ground(600,350,100,20);
+  block1 = new Block(300,275,30,40);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  block16 = new Block(390,155,30,40); 
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255);
+  Engine.update(engine);
+  ground1.display();
+  ground3.display();
+  ground2.display();
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display();
+  block15.display();
+  block16.display();
+
   drawSprites();
 }
